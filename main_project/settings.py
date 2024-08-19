b4a5ceb09593e5e5e3ef594aec9a02318937ac7d
RonyTrespalacios <rony.trespalacios@unillanos.edu.co>
2024-08-19 16:16:19 -0500

    feat : spanish settings

--- main_project/settings.py
+++ main_project/settings.py
@@ -108,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es'
 
 # Configura la zona horaria a GMT-5
 TIME_ZONE = 'America/Bogota'  # GMT-5 sin horario de verano
@@ -117,6 +117,8 @@ USE_I18N = True
 
 USE_TZ = True
 
+# Configuración de localización
+USE_L10N = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
